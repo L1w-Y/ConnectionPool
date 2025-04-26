7da90eb2ced14610c6cc31cb288d111904e7fed6
L1w137 <L1w2022@163.com>
2025-04-26 15:39:44 +0800

    :white_check_mark:压力测试

--- main.cpp
+++ main.cpp
@@ -8,16 +8,81 @@ int main()
 
 	clock_t begin = clock();
 	ConnectionPool* cp = ConnectionPool::getConnectionPool();
-	for (int i =0;i<1000;++i)
-	{
-		cp->init(config);
+	cp->init(config);
+	
+	thread t1([&]
+		{
+			auto sp = cp->getConnection();
+			if (!sp) {
+				std::cerr << "连接池没有拿到连接，线程退出\n";
+				return;
+			}
+			for (int i = 0; i < 200; ++i)
+			{
+				char sql[1024] = { 0 };
+				sprintf_s(sql, sizeof(sql),
+					"INSERT INTO user(name, age, sex) VALUES('%s', %d, '%s')",
+					"zhangshan", 20, "male");
+
+				sp->update(sql);
+			}
+		});
+
+	thread t2([&]
+		{
+			auto sp = cp->getConnection();
+			if (!sp) {
+				std::cerr << "连接池没有拿到连接，线程退出\n";
+				return;
+			}
+			for (int i = 0; i <200; ++i)
+			{
+				char sql[1024] = { 0 };
+				sprintf_s(sql, sizeof(sql),
+					"INSERT INTO user(name, age, sex) VALUES('%s', %d, '%s')",
+					"zhangshan", 20, "male");
+				sp->update(sql);
+			}
+		});
+	thread t3([&]
+		{
 		auto sp = cp->getConnection();
-		char sql[1024] = { 0 };
-		sprintf_s(sql, sizeof(sql),
-			"INSERT INTO user(name, age, sex) VALUES('%s', %d, '%s')",
-			"zhangshan", 20, "male");
-		sp->update(sql);
-	}
+		if (!sp) {
+			std::cerr << "连接池没有拿到连接，线程退出\n";
+			return;
+		}
+			for (int i = 0; i < 200; ++i)
+			{
+				char sql[1024] = { 0 };
+				sprintf_s(sql, sizeof(sql),
+					"INSERT INTO user(name, age, sex) VALUES('%s', %d, '%s')",
+					"zhangshan", 20, "male");
+				
+				sp->update(sql);
+			}
+		});
+
+	thread t4([&]
+		{
+			auto sp = cp->getConnection();
+			if (!sp) {
+				std::cerr << "连接池没有拿到连接，线程退出\n";
+				return;
+			}
+			for (int i = 0; i < 200; ++i)
+			{
+				char sql[1024] = { 0 };
+				sprintf_s(sql, sizeof(sql),
+					"INSERT INTO user(name, age, sex) VALUES('%s', %d, '%s')",
+					"zhangshan", 20, "male");
+				sp->update(sql);
+			}
+		});
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
 	clock_t end = clock();
 	cout << end - begin << "ms\n";
 
